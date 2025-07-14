e826338dfdf9b0c542dda697d00ee2c5948e2892
Andrews3002 <alexangeloandrews@gmail.com>
2025-07-14 12:07:25 -0400

    event listener and emitter excercise

--- Node.js tutorials/app.js
+++ Node.js tutorials/app.js
@@ -2,13 +2,17 @@ const EventEmitter = require('events')
 const emitter = new EventEmitter()
 
 //Register a listenter with addListener and on, 2 interchangeable functions
-emitter.addListener('messageLogged', function(){
-    console.log("Listener called")
+emitter.addListener('messageLogged', function(arg){
+    console.log("Listener called", arg)
 })
-emitter.addListener('messageLogged', function(){
-    console.log("Listener called")
+emitter.addListener('messageLogged', function(arg){
+    console.log("Listener called", arg)
+})
+emitter.addListener('messageLogged', (arg) => {
+    console.log("Listener called", arg)
 })
 
+
 //Raise an event
-emitter.emit('messageLogged')
+emitter.emit('messageLogged', {data:"message"})
 
