80f0c64be998242635cc1013cd03107aa5826132
Andrews3002 <alexangeloandrews@gmail.com>
2025-07-14 10:40:40 -0400

    commit

--- Node.js tutorials/app.js
+++ Node.js tutorials/app.js
@@ -1,7 +1,10 @@
 const EventEmitter = require('events')
 const emitter = new EventEmitter()
 
-//Register a listenter
+//Register a listenter with addListener and on, 2 interchangeable functions
+emitter.addListener('messageLogged', function(){
+    console.log("Listener called")
+})
 emitter.addListener('messageLogged', function(){
     console.log("Listener called")
 })
