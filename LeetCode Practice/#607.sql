da7f44acd8c666410967d17b5264431ba4393f63
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-09 08:50:52 -0400

    #607 attempt 3

--- LeetCode Practice/#607.sql
+++ LeetCode Practice/#607.sql
@@ -3,6 +3,6 @@ WHERE A.sales_id NOT IN (
     SELECT B.sales_id FROM Orders B
     WHERE B.com_id IN (
         SELECT C.com_id FROM Company C
-        WHERE C.name = "RED"
+        WHERE C.name = 'RED'
     )
 );
