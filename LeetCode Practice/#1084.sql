dce7dc363c4080999c915e26c3f33afbfee0aa1d
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-01-19 19:08:20 -0400

    #1084 attempt 3

--- LeetCode Practice/#1084.sql
+++ LeetCode Practice/#1084.sql
@@ -2,7 +2,10 @@ SELECT A.product_id, C.product_name
 FROM Product A
 JOIN Product C
 ON A.product_id = C.product_id
-WHERE A.product_id NOT IN(
+WHERE A.product_id IN(
     SELECT B.product_id FROM Sales B
-    WHERE B.sale_date < DATE '2019-01-01' OR B.sale_date > DATE '2019-03-31'
+    WHERE B.sale_date >= DATE '2019-01-01' OR B.sale_date <= DATE '2019-03-31'
+) AND A.product_id NOT IN(
+    SELECT D.product_id FROM Sales D
+    WHERE D.sale_date < DATE '2019-01-01' OR D.sale_date > DATE '2019-03-31'
 );
