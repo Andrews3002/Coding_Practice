c53f2b4af0acacdaafb43d1f87c6eb4ea9b4a38f
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-01-20 07:57:32 -0400

    #1141 attempt 2

--- LeetCode Practice/#1141.sql
+++ LeetCode Practice/#1141.sql
@@ -1,8 +1,6 @@
 SELECT 
-CASE
-    WHEN A.activity_date BETWEEN DATE '2019-06-27' AND '2019-07-27'
-    THEN A.activity_date
-END AS day,
+A.activity_date AS day,
 COUNT(DISTINCT A.user_id) AS active_users
 FROM Activity A
 GROUP BY (A.activity_date)
+HAVING A.activity_date BETWEEN DATE '2019-06-27' AND DATE '2019-07-27';
