8a326c8b4040d0b2967477bfdd0127bcd9fff311
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-20 14:16:06 -0400

    #1211 attempt 2

--- LeetCode Practice/#1211.sql
+++ LeetCode Practice/#1211.sql
@@ -1,6 +1,6 @@
 SELECT 
     A.query_name,
-    ROUND((SUM(A.rating::numeric / A.position::numeric) / COUNT(*)::numeric), 2) AS quality
-    -- ROUND(((COUNT(A.rating < 3) / COUNT(*)) * 100), 2) AS poor_query_percentage
+    ROUND((SUM(A.rating::numeric / A.position::numeric) / COUNT(*)::numeric), 2) AS quality,
+    ROUND((((COUNT(*) FILTER(WHERE A.rating < 3))::numeric/(COUNT(*)::numeric))*100),2) AS poor_query_percentage
 FROM Queries A
 GROUP BY (A.query_name)
