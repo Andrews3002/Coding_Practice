d3ec1ed3ca2eec621d8f149a9ff37e810bcde567
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-01-20 07:58:53 -0400

    #1141 attempt 3

--- LeetCode Practice/#1141.sql
+++ LeetCode Practice/#1141.sql
@@ -3,4 +3,4 @@ A.activity_date AS day,
 COUNT(DISTINCT A.user_id) AS active_users
 FROM Activity A
 GROUP BY (A.activity_date)
-HAVING A.activity_date BETWEEN DATE '2019-06-27' AND DATE '2019-07-27';
+HAVING A.activity_date BETWEEN DATE '2019-06-28' AND DATE '2019-07-27';
