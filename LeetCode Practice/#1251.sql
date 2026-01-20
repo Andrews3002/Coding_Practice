8402ad730c87afd09b562262bda4f3ab63dd45e6
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-20 15:05:16 -0400

    #1251 attempt 2

--- LeetCode Practice/#1251.sql
+++ LeetCode Practice/#1251.sql
@@ -4,7 +4,12 @@ SELECT
         (SUM(A.units::numeric * B.price::numeric)/SUM(A.units::numeric)),
         2
     ) AS average_price
-FROM UnitsSold A
-JOIN Prices B
-ON A.product_id = B.product_id
+FROM 
+    UnitsSold A
+JOIN 
+    Prices B
+ON 
+    A.product_id = B.product_id
+    AND
+    A.purchase_date BETWEEN B.start_date AND B.end_date
 GROUP BY a.product_id
