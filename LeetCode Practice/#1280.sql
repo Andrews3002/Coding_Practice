14f26255b0f48a430c589deb4c64ec6ba75f8cf3
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-01-21 08:38:45 -0400

    #1280 attempt 2

--- LeetCode Practice/#1280.sql
+++ LeetCode Practice/#1280.sql
@@ -1,10 +1,17 @@
-SELECT 
-    A.student_id, 
-    B.student_name,
-    A.subject_name,
-    COUNT(*) AS attended_exams
-FROM Examinations A
-JOIN Students B ON A.student_id = B.student_id
-JOIN Subjects C ON A.subject_name = C.subject_name
-GROUP BY (A.student_id, B.student_name, A.subject_name)
-ORDER BY (A.student_id, A.subject_name) ASC
+SELECT
+    A.student_id,
+    A.student_name,
+    B.subject_name,
+    COALESCE(C.attendance, 0) AS attended_exams
+FROM Students A
+CROSS JOIN Subjects B
+LEFT JOIN (
+    SELECT 
+        student_id, 
+        subject_name,
+        COUNT(*) AS attendance
+    FROM Examinations
+    GROUP BY (student_id, subject_name)
+) AS C
+ON (A.student_id, B.subject_name) = (C.student_id, C.subject_name)
+ORDER BY (A.student_id, B.subject_name) ASC
