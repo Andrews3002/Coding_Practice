5df768d8467499dc2ceda2b821fa108932ac443f
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-01-24 19:19:45 -0400

    #1407 attempt 1

---
+++ LeetCode Practice/#1407.sql
@@ -0,0 +1,13 @@
+SELECT
+    A.name,
+    COALESCE(B.distance, 0)
+FROM Users A
+LEFT JOIN (
+    SELECT 
+        user_id, 
+        SUM(distance) AS distance
+    FROM Rides
+    GROUP BY (user_id)
+) AS B
+ON A.id = B.user_id
+ORDER BY B.distance DESC, A.name ASC)
