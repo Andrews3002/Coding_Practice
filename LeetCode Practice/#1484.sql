e3206b41604e14f19d1e1a7dad47c273b06f40cc
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-01-25 07:43:16 -0400

    #1484 attempt 2

--- LeetCode Practice/#1484.sql
+++ LeetCode Practice/#1484.sql
@@ -1,7 +1,7 @@
 SELECT
     sell_date,
     COUNT(*) AS num_sold,
-    string_agg(product, ',', ORDER BY product ASC) AS products
+    string_agg(product, ', ' ORDER BY product ASC) AS products
 FROM Activities
 GROUP BY sell_date
 ORDER BY sell_date ASC;
