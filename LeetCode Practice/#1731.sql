e0568f60d9c4d0c7f26539b95b9e49b443edd866
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-27 16:03:56 -0400

    #1731 attempt 1

---
+++ LeetCode Practice/#1731.sql
@@ -0,0 +1,10 @@
+SELECT 
+    A.employee_id,
+    A.name,
+    COUNT(*) AS reports_count,
+    ROUND(AVG(B.age::numeric), 0) AS average_age
+FROM Employees A
+INNER JOIN Employees B
+ON A.employee_id = B.reports_to
+GROUP BY A.employee_id
+ORDER BY A.employee_id ASC
