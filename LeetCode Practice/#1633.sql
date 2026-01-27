ba150b8d56fb8067feef7056c9e1a4c30606be29
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-27 10:35:58 -0400

    #1633 attempt 2

--- LeetCode Practice/#1633.sql
+++ LeetCode Practice/#1633.sql
@@ -1,12 +1,5 @@
 SELECT 
     contest_id,
-    ROUND(((COUNT(*)::numeric)/MAX(A.rn)::numeric)*100 ,2) AS percentage
+    ROUND(((COUNT(*)::numeric)/(SELECT COUNT(*) FROM Users))::numeric)*100 ,2) AS percentage
 FROM Register
-JOIN (
-    SELECT 
-        user_id, 
-        ROW_NUMBER() OVER (ORDER BY user_id ASC) AS rn
-    FROM Users
-) AS A
-ON user_id = A.user_id
 GROUP BY contest_id
