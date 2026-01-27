691d228a01d6b7e260d25a3293c0d9fac1e37a9c
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-27 10:36:56 -0400

    #1633 attempt 3

--- LeetCode Practice/#1633.sql
+++ LeetCode Practice/#1633.sql
@@ -1,5 +1,5 @@
 SELECT 
     contest_id,
-    ROUND(((COUNT(*)::numeric)/(SELECT COUNT(*) FROM Users))::numeric)*100 ,2) AS percentage
+    ROUND((((COUNT(*)::numeric)/(SELECT COUNT(*) FROM Users))::numeric)*100 ,2) AS percentage
 FROM Register
 GROUP BY contest_id
