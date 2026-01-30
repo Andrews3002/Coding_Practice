a32b1e619fe16392144efc8b6542385c7cfc41ca
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-30 18:30:44 -0400

    #1890 attempt 2

--- LeetCode Practice/#1890.sql
+++ LeetCode Practice/#1890.sql
@@ -2,8 +2,8 @@ SELECT
     user_id,
     MAX(datetime) AS last_stamp
 FROM Logins
-GROUP BY user_id
 WHERE user_id IN (
     SELECT user_id FROM Logins
     WHERE datetime >= '2021-01-01'
-);
+)
+GROUP BY user_id
