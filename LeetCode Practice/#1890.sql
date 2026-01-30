efebba291b5860eb17be707c5952d58453fa4831
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-30 18:38:00 -0400

    #1890 attempt 4

--- LeetCode Practice/#1890.sql
+++ LeetCode Practice/#1890.sql
@@ -4,6 +4,7 @@ SELECT
 FROM Logins
 WHERE user_id IN (
     SELECT user_id FROM Logins
-    WHERE time_stamp >= '2020-01-01'
+    WHERE time_stamp >= DATE'2020-01-01'
+    AND time_stamp <= DATE'2020-12-31'
 )
 GROUP BY user_id
