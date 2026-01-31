d0cf49e41275e945d671c24c5468768d8a7969b1
Andrews3002 <alexangeloandrews@gmail.com>
2026-01-31 15:27:21 -0400

    #1890 attempt 6

--- LeetCode Practice/#1890.sql
+++ LeetCode Practice/#1890.sql
@@ -1,5 +1,6 @@
 SELECT
     user_id,
-    MAX(time_stamp WHERE times_stamp BETWEEN DATE'2020-01-01' AND DATE'2020-12-31') AS last_stamp
+    MAX(time_stamp) AS last_stamp
 FROM Logins
+WHERE times_stamp BETWEEN DATE'2020-01-01' AND DATE'2020-12-31'
 GROUP BY user_id
