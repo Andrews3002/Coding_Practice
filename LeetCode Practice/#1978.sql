786ff2fd63cfae40e81ca7017e817e221c7dbfaf
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-02-02 06:13:16 -0400

    #1978 attempt 7

--- LeetCode Practice/#1978.sql
+++ LeetCode Practice/#1978.sql
@@ -1,6 +1,6 @@
 SELECT employee_id FROM Employees
-WHERE (manager_id IS NOT NULL
+WHERE manager_id IS NOT NULL
 AND manager_id NOT IN (
     SELECT employee_id FROM Employees
-))
-OR (salary < 30000)
+)
+AND salary < 30000
