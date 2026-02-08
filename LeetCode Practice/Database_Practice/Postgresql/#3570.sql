aba0250956277ee39c3f096776c1269cd6e7a115
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-02-08 19:39:50 -0400

    #3570 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#3570.sql
@@ -0,0 +1,18 @@
+SELECT 
+    book_id,
+    title,
+    author,
+    genre,
+    publication_year,
+    COALESCE(B.total_borrowed, 0) AS current_borrowers
+FROM library_books
+LEFT JOIN (
+    SELECT 
+        book_id,
+        COUNT(*) AS total_borrowed
+    FROM borrowing_records
+    WHERE return_date IS NULL
+) AS B
+ON book_id = B.book_id
+WHERE total_copies = B.total_borrowed
+ORDER BY current_borrowers DESC title ASC
