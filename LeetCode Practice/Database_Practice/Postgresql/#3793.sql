d1e310c9c9efb9d7a475ab0b7b05ec2cc71e35ed
Alexangelo Andews <alexangeloandrews@gmail.com>
2026-02-09 08:52:12 -0400

    #3793 attempt 5

--- LeetCode Practice/Database_Practice/Postgresql/#3793.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#3793.sql
@@ -3,6 +3,6 @@ SELECT
     COUNT(DISTINCT prompt) AS prompt_count,
     ROUND((SUM(tokens::numeric)/COUNT(DISTINCT prompt)::numeric), 2) AS avg_tokens
 FROM prompts
-WHERE promp_count >= 3
+WHERE COUNT(DISTINCT prompt) >= 3
 GROUP BY user_id
 ORDER BY avg_tokens DESC, user_id ASC
