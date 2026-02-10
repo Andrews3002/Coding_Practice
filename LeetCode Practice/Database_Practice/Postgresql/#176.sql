77b91ce129adae69c472ac47fa6e1a19b0e5b603
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-10 06:59:02 -0400

    #176 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#176.sql
@@ -0,0 +1,10 @@
+SELECT
+    COALESCE(A.salary, NULL) AS SecondHighestSalary
+FROM (
+    SELECT
+        salary,
+        ROW_NUMBER() OVER(ORDER BY salary DESC) AS rn
+    FROM Employee
+    GROUP BY salary
+) AS A
+WHERE A.rn = 2
