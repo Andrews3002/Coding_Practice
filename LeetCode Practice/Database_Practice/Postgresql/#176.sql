88e89311434efac79dcfbb87f20eb1a56ca27dae
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-10 07:31:48 -0400

    #176 attempt 8

--- LeetCode Practice/Database_Practice/Postgresql/#176.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#176.sql
@@ -1,13 +1,7 @@
 SELECT
-    CASE
-        WHEN A.rn = 2 THEN salary
-        ELSE NULL
-    END AS SecondHighestSalary
-FROM (
-    SELECT
-        salary,
-        ROW_NUMBER() OVER(ORDER BY salary DESC) AS rn
-    FROM Employee
-    GROUP BY salary
-) AS A
-WHERE A.rn = 2
+    salary AS "SecondHighestSalary"
+FROM Employee
+GROUP BY salary
+ORDER BY salary DESC
+OFFSET = 1
+LIMIT = 1
