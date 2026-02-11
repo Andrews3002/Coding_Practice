93805415f319664167afc9397b82750657ed1161
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-11 10:50:59 -0400

    #180 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#180.sql
@@ -0,0 +1,11 @@
+SELECT
+    A.num AS "ConsecutiveNumbers"
+FROM Logs A, Logs B, Logs C
+WHERE 
+    A.num = B.num
+    AND
+    B.num = C.num
+    AND 
+    A.id = B.id-1
+    AND
+    B.id = C.id-1
