d4e504b640cf0124c6376d1d7b7a2d6090f22aee
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-11 06:07:30 -0400

    #177 attempt 5

--- LeetCode Practice/Database_Practice/Postgresql/#177.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#177.sql
@@ -1,12 +1,14 @@
 CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
 BEGIN
+  IF N <= 0 THEN
+    RETURN QUERY(
+        SELECT NULL::INT;
+    )
+  ELSE
   RETURN QUERY (
     -- Write your PostgreSQL query statement below.
     SELECT
-        CASE
-            WHEN N > 0 THEN E.salary
-            ELSE NULL
-        END AS "salary"
+        E.salary AS "salary"
     FROM Employee E
     GROUP BY E.salary
     ORDER BY E.salary DESC
