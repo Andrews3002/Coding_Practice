dd4615ec4eec117d709ff39c288440cdb5d98f82
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-13 13:38:30 -0400

    #550 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#550.sql
@@ -0,0 +1,19 @@
+SELECT
+    ROUND((SUM(C.condition)::numeric/COUNT(DISTINCT C.player_id)::numeric),2) AS "fraction"
+FROM (
+    SELECT
+        A.player_id AS "player_id",
+        CASE
+            WHEN A.event_date = B.first_day + 1 THEN 1
+            ELSE 0
+        END AS "condition"
+    FROM Activity
+    JOIN (
+        SELECT
+            player_id,
+            MIN(event_date) AS first_day
+        FROM Activity
+        GROUP BY player_id
+    ) AS B
+    ON A.player_id = B.player_id
+) AS C
