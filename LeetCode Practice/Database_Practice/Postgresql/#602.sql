3d240a7e2f6c0c69059ddc1efa27c9999d679280
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-20 13:31:36 -0400

    #602 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#602.sql
@@ -0,0 +1,16 @@
+SELECT
+    A.id AS "id",
+    COUNT(*) AS "num"
+FROM (
+    SELECT
+        requester_id AS "id"
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT
+        accepter_id AS "id"
+    FROM RequestAccepted
+) AS A
+GROUP BY A.id
+HAVING MAX(COUNT(*))
