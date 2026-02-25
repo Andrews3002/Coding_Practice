79a6b1ea221eb3f5aad813da46b77947741a7096
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-25 10:43:47 -0400

    #608 attempt 4

--- LeetCode Practice/Database_Practice/Postgresql/#608.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#608.sql
@@ -1,13 +1,13 @@
 SELECT
     A.id AS "id",
     CASE
-        WHEN A.p_id IS NULL THEN 'ROOT'
+        WHEN A.p_id IS NULL THEN 'Root'
         WHEN (
             SELECT
                 COUNT(*)
             FROM Tree B
             WHERE B.p_id = A.id
-        ) = 0 THEN 'LEAF'
-        ELSE 'INNER'
+        ) = 0 THEN 'Leaf'
+        ELSE 'Inner'
     END AS "type"
 FROM Tree A
