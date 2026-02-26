21c807a921db6fee5805641ba3d593922f86055f
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-26 07:27:03 -0400

    #626 attempt 3

--- LeetCode Practice/Database_Practice/Postgresql/#626.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#626.sql
@@ -1,11 +1,12 @@
 SELECT
+    PRESENT.id AS "id",
     CASE 
         WHEN FUTURE.student IS NULL THEN PRESENT.student
         WHEN PRESENT.id%2 = 0 THEN PAST.student
         ELSE FUTURE.student
     END AS "student"
 FROM Seat PRESENT
-JOIN Seat PAST 
+LEFT JOIN Seat PAST 
 ON PRESENT.id-1 = PAST.id
-JOIN Seat FUTURE
+LEFT JOIN Seat FUTURE
 ON PRESENT.id+1 = FUTURE.id
