a9c8eead24d44a65a8ca873f28edf2f112087022
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-27 12:16:44 -0400

    #1045 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#1045.sql
@@ -0,0 +1,22 @@
+SELECT
+    CASE
+        WHEN (
+            SELECT COUNT(*)
+            FROM Product
+        ) = A.cCount THEN A.customer_id
+        ELSE NULL
+    END AS 'customer_id'
+FROM (
+    SELECT
+        B.customer_id AS "customer_id",
+        COUNT(*) AS "cCount"
+    FROM (
+        SELECT
+            customer_id,
+            product_key
+        FROM Customer
+        GROUP BY customer_id, product_key
+    ) AS B
+    GROUP BY B.customer_id
+) AS A
+WHERE cutomer_id IS NOT NULL
