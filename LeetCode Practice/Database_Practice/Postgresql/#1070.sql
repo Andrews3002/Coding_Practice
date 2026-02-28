f42dee66adaaee202ed77256087db72ed6f4fa0e
Andrews3002 <alexangeloandrews@gmail.com>
2026-02-28 09:37:54 -0400

    #1070 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#1070.sql
@@ -0,0 +1,15 @@
+SELECT
+    A.product_id, AS "product_id",
+    A.year AS "first_year",
+    A.quantity AS "quantity",
+    A.price AS "price"
+FROM Sales A
+JOIN (
+    SELECT
+        product_id,
+        MIN(year) AS intro_year
+    FROM Sales
+    GROUP BY product_id
+) AS B
+ON A.product_id = B.product_id
+WHERE A.year = B.intro_year
