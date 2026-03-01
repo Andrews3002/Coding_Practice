c435321115d5cb33eeb59f02706ec9293f6bb3ea
Andrews3002 <alexangeloandrews@gmail.com>
2026-03-01 05:48:07 -0400

    #1158 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#1158.sql
@@ -0,0 +1,20 @@
+SELECT 
+    A.user_id AS "buyer_id",
+    A.join_date AS "join _date"
+    COALESCE(B.total_orders, 0) AS "orders_in_2019"
+FROM Users A
+WHRERE A.user_id IN (
+    SELECT
+        buyer_id
+    FROM Orders
+    GROUP BY buyer_id
+)
+JOIN (
+    SELECT 
+        buyer_id AS "buyer_id"
+        COUNT(*) AS "total_orders"
+    FROM Orders
+    WHERE order_date > DATE "2019-01-01" AND order_date < DATE "2020-01-01"
+    GROUP BY buyer_id
+) AS B
+ON A.user_id = B.buyer_id
