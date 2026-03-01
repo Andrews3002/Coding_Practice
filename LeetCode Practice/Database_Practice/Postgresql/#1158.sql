0d51001e332a50010932b76661d9badc90fbbc9a
Andrews3002 <alexangeloandrews@gmail.com>
2026-03-01 06:02:54 -0400

    #1158 attempt 9

--- LeetCode Practice/Database_Practice/Postgresql/#1158.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#1158.sql
@@ -1,6 +1,6 @@
 SELECT 
     A.user_id AS "buyer_id",
-    A.join_date AS "join _date",
+    A.join_date AS "join_date",
     COALESCE(B.total_orders, 0) AS "orders_in_2019"
 FROM Users A
 LEFT JOIN (
