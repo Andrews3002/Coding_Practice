cbe20efb3d569de3bc60c2bd088a1ec59a173af5
Andrews3002 <alexangeloandrews@gmail.com>
2026-03-04 10:56:07 -0400

    #1193 attempt 3

--- LeetCode Practice/Database_Practice/Postgresql/#1193.sql
+++ LeetCode Practice/Database_Practice/Postgresql/#1193.sql
@@ -2,9 +2,9 @@ SELECT
     B.month AS "month",
     B.country AS "country",
     COUNT(*) AS "trans_count",
-    COUNT(*) FILTER(WHERE B.state = "approved") AS "approved_count",
+    COUNT(*) FILTER(WHERE B.state = 'approved') AS "approved_count",
     SUM(amount) AS "trans_total_amount",
-    SUM(amount) FILTER(WHERE B.state = "approved") AS "approved_total_amount"
+    SUM(amount) FILTER(WHERE B.state = 'approved') AS "approved_total_amount"
 FROM (
     SELECT
         A.id AS "id",
