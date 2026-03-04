23af6354fc0f09565966938a591d0d52e532c4d2
Andrews3002 <alexangeloandrews@gmail.com>
2026-03-04 10:54:19 -0400

    #1193 attempt 1

---
+++ LeetCode Practice/Database_Practice/Postgresql/#1193.sql
@@ -0,0 +1,17 @@
+SELECT 
+    B.month AS "month",
+    B.country AS "country",
+    COUNT(*) AS "trans_count",
+    COUNT(*) FILTER(WHERE B.state = "approved") AS "approved_count",
+    SUM(amount) AS "trans_total_amount",
+    SUM(amount) FILTER(WHERE B.state = "approved") AS "approved_total_amount"
+FROM (
+    SELECT
+        A.id AS "id",
+        A.country AS "country",
+        A.state AS "state",
+        A>amount AS "amount",
+        TO_CHAR(A.trans_date, 'YYYY-MM') AS "month"
+    FROM Transactions A
+) AS B
+GROUP BY B.month, B.country
