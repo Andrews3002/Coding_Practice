0e0c0cdfb734e7cbda0dfc508a244d06aa876980
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-16 17:56:41 -0400

    #1934 attempt 8

--- LeetCode Practice/Database_Practice/#1934.sql
+++ LeetCode Practice/Database_Practice/#1934.sql
@@ -1,6 +1,6 @@
 SELECT
     s.user_id AS "user_id",
-    NVL(ROUND((SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)/NULLIF(SUM(CASE WHEN c.action IN ('timeout', 'confirmed') THEN 1 ELSE 0 END), 0)), 2), 0) AS "confirmation_rate"
+    ROUND(AVG(CASE WHEN c.action IN ('confirmed') THEN 1 ELSE 0 END), 2) AS "confirmation_rate"
 FROM signups s
 LEFT JOIN confirmations c
 ON s.user_id = c.user_id
