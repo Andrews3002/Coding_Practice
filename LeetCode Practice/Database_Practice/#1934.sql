d6869d624d6c7607c842aaa4ce66b02d6b808703
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-16 17:33:01 -0400

    #1934 attempt 6

--- LeetCode Practice/Database_Practice/#1934.sql
+++ LeetCode Practice/Database_Practice/#1934.sql
@@ -2,6 +2,6 @@ SELECT
     c.user_id,
     ROUND((SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)/SUM(CASE WHEN c.action IN ('timeout', 'confirmed') THEN 1 ELSE 0 END)), 2) AS "confirmation_rate"
 FROM signups s
-JOIN confirmations c 
+LEFT JOIN confirmations c 
 ON s.user_id = c.user_id
 GROUP BY c.user_id;
