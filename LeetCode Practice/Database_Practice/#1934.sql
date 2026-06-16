136ffa46fdc50845cb93ad0845ce21cb8cddd9c0
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-16 17:22:11 -0400

    #1934 attempt 1

---
+++ LeetCode Practice/Database_Practice/#1934.sql
@@ -0,0 +1,8 @@
+SELECT
+    s.user_id,
+    ROUND((SUM(WHEN c.action = "confirmed" THEN 1 ELSE 0 END)/SUM(WHEN c.action IN ("timeout", "confirmed") THEN 1 ELSE 0 END)), 2) AS "confirmation_rate"
+FROM signups s
+JOIN confirmations c 
+ON s.user_id = c.user_id
+GROUP BY c.user_id;
+/
