e804db37e47fbfa900f4627d8629e1447fda09b3
Andrews3002 <alexangeloandrews@gmail.com>
2026-06-20 11:55:51 -0400

    #3421 attempt 3

--- LeetCode Practice/Database_Practice/#3421.sql
+++ LeetCode Practice/Database_Practice/#3421.sql
@@ -1,8 +1,19 @@
 SELECT
     a.student_id AS "student_id",
     a.subject AS "subject",
-    MIN(a.exam_date) AS "first_exam_date",
-    MAX(a.exam_date) AS "last_exam_date"
-FROM scores a
-GROUP BY a.student_id, a.subject
-HAVING MIN(a.exam_date) != MAX(a.exam_date)
+    b.score AS "first_score",
+    c.score AS "latest_score"
+FROM (
+    SELECT
+        student_id AS student_id,
+        subject AS subject,
+        MIN(exam_date) AS first_exam_date,
+        MAX(exam_date) AS last_exam_date
+    FROM scores
+    GROUP BY student_id, subject
+    HAVING MIN(exam_date) != MAX(exam_date)
+) a
+JOIN scores b
+ON a.student_id = b.student_id AND a.subject = b.subject AND a.first_exam_date = b.exam_date
+JOIN scores c
+ON a.student_id = c.student_id AND a.subject = c.subject AND a.last_exam_date = c.exam_date
